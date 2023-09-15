24efc683ad3594981b3e5de8fd78e2bf29430bf4
Sanket <alanesanket8@gmail.com>
2023-09-15 16:14:57 +0530

    minconstant

---
+++ firstbad.py
@@ -0,0 +1,9 @@
+def firstBadVersion(n: int) -> int:
+        left,right=0,n-1
+        while left <= right:
+            mid = (left+right)//2
+            if isBadVersion(mid)==False:
+                left=mid+1
+            else:
+                right=mid-1    
+        return left
