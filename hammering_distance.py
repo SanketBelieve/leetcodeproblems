f0abcf35345efba1f231197ef5068bfe3d20813d
Sanket <alanesanket8@gmail.com>
2023-09-25 09:54:05 +0530

    hammingdis

---
+++ hammering_distance.py
@@ -0,0 +1,7 @@
+def hammingDistance(self, x: int, y: int) -> int:
+        ans=0
+        for i in range(31,-1,-1):
+            b1=x>>i & 1
+            b2=y>> i & 1
+            ans += not (b1==b2)
+        return ans    
