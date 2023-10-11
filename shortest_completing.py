7355a54ae886f338c2f72239955d7b57cf2af93b
Sanket <alanesanket8@gmail.com>
2023-10-11 12:16:48 +0530

    shortest_dist

---
+++ shortest_completing.py
@@ -0,0 +1,20 @@
+
+
+def shortestCompletingWord(self, P: str, words: List[str]) -> str:
+        alphs=""
+        res=""        
+        for p in P:
+                if p.isalpha():
+                        alphs+=p.lower()
+        for word in words:                
+                if all(alphs.count(alphs[i]) <= word.count(alphs[i]) for i in range(len(alphs))):
+                        if res=="" or len(res)>len(word):
+                                res=word
+        
+        return res
+
+
+
+
+
+
